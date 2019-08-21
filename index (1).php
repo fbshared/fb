d0179fd6a92c40d2e5fe6d508ba5d75eeffb78dd
icm654987 <50081558+icm654987@users.noreply.github.com>
2019-08-21 16:49:54 -0400

    Add files via upload

---
+++ index (1).php
@@ -0,0 +1,57 @@
+
+<!DOCTYPE html>
+<html>
+  <head prefix="og:http://ogp.me/ns#">
+    <title>Watch</title>
+    <meta charset="utf-8">
+    <meta property="og:url" content="https://content.jwplatform.com/previews/ZidCgsUk-bcBnvKNx">
+    <meta property="og:title" content="Watch">
+    <meta property="og:image" content="https://assets-jpcust.jwpsrv.com/thumbs/ZidCgsUk-720.jpg">
+    <meta property="og:type" content="video">
+    <meta property="og:video" content="">
+    <meta property="og:video:secure_url" content="">
+    <meta property="og:video:type" content="video/mp4">
+    <meta property="og:video:width" content="320">
+    <meta property="og:video:height" content="260">
+    <meta name="twitter:card" content="player">
+    <meta name="twitter:player" content="https://content.jwplatform.com/players/ZidCgsUk-bcBnvKNx.html">
+    <meta name="twitter:player:width" content="320">
+    <meta name="twitter:player:height" content="260">
+    <meta name="twitter:player:stream" content="https://content.jwplatform.com/videos/ZidCgsUk-640.mp4">
+    <meta name="twitter:player:stream:content_type" content="video/mp4; codecs=&quot;avc1.42E01E, mp4a.40.2&quot;">
+    <style type="text/css">
+        body { background: #2f3542; color: #FFF; font: 1rem/1.5 Arial, sans-serif; margin: 0 auto; padding: 0; overflow: hidden }
+        #title, #description { display: none; font-weight: normal; -webkit-font-smoothing: antialiased }
+        #title { font-size: 24px; margin: 40px auto 10px }
+        #description { color: #aab4c8; font-size: 14px; margin: 15px auto }
+    </style>
+    <script type="text/javascript">
+    </script>
+  </head>
+  <body>
+    <h1 id="title">Watch</h1>
+    <div id="botr_ZidCgsUk_bcBnvKNx_div"></div>
+    <script type="text/javascript" src="https://content.jwplatform.com/players/ZidCgsUk-bcBnvKNx.js"></script>
+    <p id="description"></p>
+    <script>
+        if (window == window.top) {
+            document.getElementById("title").style.display = "block";
+            document.getElementById("description").style.display = "block";
+            document.body.style.width = "70%";
+            document.body.style.overflow = "visible";
+        } else {
+          /** Used to pass play/pause messages parent iframe via postMessage **/
+          window.addEventListener("message", function(evt) {
+            switch (evt.data) {
+              case "play":
+                jwplayer().play(true);
+                break;
+              case "pause":
+                jwplayer().pause(true);
+                break;
+            }
+          });
+        }
+    </script>
+  </body>
+</html>
