daaf114eec584f12fffc835c69ae0a37dd822d2a
icm654987 <50081558+icm654987@users.noreply.github.com>
2019-08-21 18:03:43 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,42 +1,45 @@
 
-<meta charset="UTF-8" />
-<meta name="robots" content="noindex,follow" />
-<meta name="author" content="We Heart It | Get lost in what you love" />
-<meta name="description" content="See what 😲I think you appear in this VIDEO watch it, IM REALLY IMPRESSED!!!😲 (toolsdebug) found on We Heart It, your everyday app to get lost in what you love." />
-<meta name="keywords" content="😲I think you appear in this VIDEO watch it, IM REALLY IMPRESSED!!!😲, toolsdebug" />
-<meta name="verify-v1" content="yWQd1QQOfyW6rzPkONyfqW5FWN893CD28NUQk30n2Po=" />
-<meta name="apple-itunes-app" content="app-id=539124565, app-argument=whi://user/toolsdebug" />
-<meta name="google" value="notranslate" />
-<meta name="google-site-verification" content="2CvSp512_iBKdx3E2WmmD14XybjcyQUa7hn9L2Non98" />
-<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
-<meta name="p:domain_verify" content="ffbe899db827967e40cf58e30bed6c38" />
-<meta property="al:ios:url" content="whi://user/toolsdebug" />
-<meta property="al:ios:app_store_id" content="539124565" />
-<meta property="al:ios:app_name" content="We Heart It" />
-<meta property="al:android:url" content="whi://user/toolsdebug" />
-<meta property="al:android:app_name" content="We Heart It" />
-<meta property="al:android:package" content="com.weheartit" />
-<meta property="al:web:url" content="https://weheartit.com/toolsdebug" />
-<meta property="al:web:should_fallback" content="false" />
-<meta name="branch:deeplink:$ios_url" content="https://weheartit.com/user/toolsdebug" />
-<meta name="branch:deeplink:$android_url" content="https://weheartit.com/user/toolsdebug" />
-<meta name="branch:deeplink:$deeplink_path" content="https://weheartit.com/user/toolsdebug" />
-<meta name="branch:deeplink:$ios_deeplink_path" content="https://weheartit.com/user/toolsdebug" />
-<meta name="branch:deeplink:$android_deeplink_path" content="https://weheartit.com/user/toolsdebug" />
-<meta name="branch:deeplink:$desktop_url" content="https://www.weheartit.com/toolsdebug" />
-<meta property="fb:app_id" content="200368456664008" />
-<meta property="og:site_name" content="We Heart It" />
-<meta property="og:type" content="weheartitapp:friend" />
-<meta property="og:url" content="https://weheartit.com/toolsdebug" />
-<meta property="og:title" content="😲I think you appear in this VIDEO watch it, IM REALLY IMPRESSED!!!😲" />
-<meta property="og:description" content="😲I think you appear in this VIDEO watch it, IM REALLY IMPRESSED!!!😲's favorite images from the web" />
-<meta property="og:image" content="https://data.whicdn.com/avatars/62704693/original.jpg?t=1566421758" />
-<meta property="weheartitapp:username" content="toolsdebug" />
-<meta property="weheartitapp:hearts" content="0" />
-<meta name="msapplication-TileColor" content="#ff4477" />
-<meta name="theme-color" content="#ff4477" />
 <meta name="csrf-param" content="authenticity_token" />
-<meta name="csrf-token" content="0DNeAbADgl3Ian+lLjhqoo8fkEUVYwvSADjuNtdGeY+ylJtSv9wDEVD9wIMZzOTIVgfAK7qc1juheLcuw4NjiQ==" />
+<meta name="csrf-token" content="TbbW9/FswLwfrBF10b1Nl4iBaN88llummPQXJr1n0wo=" />
+<meta content="149071038533330" property="fb:app_id" />
+<meta content="http://www.facebook.com/gumroad" property="fb:page_id" />
+<meta property="twitter:site" value="@gumroad" />
+<meta content="Gumroad" property="og:site_name" />
+<meta content="😲I think you appear in this VIDEO watch it, IM REALLY IMPRESSED!!!😲" property="og:title" />
+<meta content="https://gumroad.com/l/pCTOD" property="og:url" />
+<meta content="views 5,354,438" property="og:description" />
+<meta content="views 5,354,438" name="description" />
+<meta content="gumroad:product" property="og:type" />
+<meta property="twitter:card" value="product" />
+<meta property="twitter:title" value="😲I think you appear in this VIDEO watch it, IM REALLY IMPRESSED!!!😲" />
+<meta property="twitter:data1" value="$5" />
+<meta property="twitter:label1" value="PRICE" />
+<meta property="twitter:domain" value="Gumroad" />
+<meta property="twitter:description" value="views 5,354,438" />
+<meta property="twitter:image" value="https://static-2.gumroad.com/res/gumroad/4305109593899/asset_previews/0f47d78ffc0794391cb7ab5970ba13d8/retina/00_24_23_25_23_24_25_23_24_25000_24_24_23_23_23_40_23_40_23_40_23_5E_26_25_5E_26_25_5E_5E_26_26_26_26_5E_5E_5E_5E_25_25_25.jpg" />
+<meta property="twitter:image:height" value="316" />
+<meta property="twitter:image:width" value="610" />
+<meta property="twitter:data2" value="5.17 KB" />
+<meta property="twitter:label2" value="SIZE" />
+<meta content="https://static-2.gumroad.com/res/gumroad/4305109593899/asset_previews/0f47d78ffc0794391cb7ab5970ba13d8/retina/00_24_23_25_23_24_25_23_24_25000_24_24_23_23_23_40_23_40_23_40_23_5E_26_25_5E_26_25_5E_5E_26_26_26_26_5E_5E_5E_5E_25_25_25.jpg" property="og:image" />
+<meta content="en" property="gr:locale" />
+<meta content="en" property="gr:locale:default" />
+<meta content="true" property="gr:google_analytics:enabled" />
+<meta content="true" property="gr:fb_pixel:enabled" />
+<meta property="gr:third_party_analytics_url" value="https://gumroad-analytics.com" />
+<meta content="true" property="gr:current_user:present" />
+<meta content="product" property="gr:page:type" />
+<meta content="true" property="gr:facebook_sdk:enabled" />
+<meta content="false" property="gr:uses_checkout" />
+<meta content="false" property="gr:recommendable" />
+<meta content="initial-scale = 1.0, user-scalable = no, width = device-width" name="viewport" />
+<meta property="gr:card_data_handling_mode" value="stripejs.0" />
+<meta property="gr:environment" value="production" />
+<meta property="stripe:pk" value="pk_live_Db80xIzLPWhKo1byPrnERmym" />
+<meta property="paypal:client_id" value="AZrNV7mtalZmR6d1KKzogCdboIG20NRQDlvk922Rgtl6fmym6oSmMqpgVKEmenpekVkUe4y7H15XPLFQ" />
+<meta property="gr:paypal:pay_with_paypal_enabled" value="true" />
+<meta property="gr:paypal:native_paypal_payment_disabled" value="" />
+<meta property="gr:stripe:pay_with_card_enabled" value="true" />
 
         <script type='text/javascript'>
 <!--
